4183a4e67b39f3f6df8c0d65a1910fd3551a52ba
Randomgituser69 <159695185+Randomgituser69@users.noreply.github.com>
2024-09-24 12:05:01 +0000

    c c

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const toggleBtn = document.querySelector('.hamburg')
+const navLinks = document.getElementById('mobile')
 
+toggleBtn.addEventListener('click', function() {
+    navLinks.classList.toggle('active')
+})
