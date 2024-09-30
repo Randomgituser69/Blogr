03f2969c43d7599f1a43b578818ffc36181daf43
Randomgituser69 <159695185+Randomgituser69@users.noreply.github.com>
2024-09-30 14:37:30 +0600

    Delete code directory

--- code/script.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.querySelector('.hamburg')
-const btn = document.querySelectorAll('.mobiles')
-const navLinkies = document.querySelector('ul')
-
-btn.addEventListener('click', function() {
-   navLinkies.classList.toggle('show')
-})
